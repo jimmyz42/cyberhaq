35f51185ce0202b356d31ed9791d41a4fa372b4b
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-04-08 23:55:25 -0400

    First prototype for bank account

---
+++ accounts.js
@@ -0,0 +1,8 @@
+var bank_account = {
+  "security question 1": "Who was your first boyfriend/girlfriend?",
+  "security answer 1": "Lucy",
+  "security question 2": "In what city were you born?",
+  "security answer 2": "Portland",
+  "security question 3": "What is your favorite movie?",
+  "security answer 3": "Mulan",
+};
