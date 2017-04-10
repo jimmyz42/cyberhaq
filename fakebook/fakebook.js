fd46f00668f4cef063740870bba3d72c16fc9eb7
Raul Boquin <raulboquin@dhcp-18-189-60-160.dyn.mit.edu>
2017-04-10 13:58:14 -0400

    Added fakebook folder and edited index.html to add fakebook URL

---
+++ fakebook/fakebook.js
@@ -0,0 +1,16 @@
+var messageTexts = [
+  "WE WILL MEET AT THE STARBUCKS ON 120 BROADWAY. I AM WEARING 2 HATS",
+  "MY NAME IS JACK POTT, AKA IHEARTCRACK420. MEET ME IN THE STARBUCKS IN KENDALL SQ",
+  "IHEARTCRACK420 HERE. LET US MEET IN THE CAFE IN THE KENDALL MARIOTT LOBBY FOR OUR DEAL.",
+];
+
+function myFunction() {
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
