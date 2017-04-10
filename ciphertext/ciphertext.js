6e63a098b3a537827cf1b40dc241cb2a1c8b6459
Elise <eyxue@mit.edu>
2017-04-10 18:37:26 -0400

    reformatted cipher game to make it easier

--- ciphertext/ciphertext.js
+++ ciphertext/ciphertext.js
@@ -1,7 +1,9 @@
 var messageTexts = [
-  "WE WILL MEET AT THE STARBUCKS ON 120 BROADWAY. I AM WEARING 2 HATS",
-  "MY NAME IS JACK POTT, AKA IHEARTCRACK420. MEET ME IN THE STARBUCKS IN KENDALL SQ",
-  "IHEARTCRACK420 HERE. LET US MEET IN THE CAFE IN THE KENDALL MARIOTT LOBBY FOR OUR DEAL.",
+  "WE WILL MEET AT THE STARBUCKS ON BROADWAY. I WILL BE WEARING GOLD SUNGLASSES SMOKING WEED.",
+  "MY NAME IS JACK POTT, AKA IHEARTCRACK. MEET ME IN THE STARBUCKS IN KENDALL SQ",
+  "IHEARTCRACK HERE. LET US MEET IN THE CAFE IN THE KENDALL MARIOTT LOBBY FOR OUR DEAL. BRING THE STUFF.",
+  "THIS IS IHEARTCRACK. I WILL CALL YOU FROM THE STARBUCKS IN KENDALL AT FOUR TWENTY.",
+  "WILL BE WAITING IN MY CAR IN FRONT OF THE STARBUCKS IN KENDALL SQ. I WILL BE SNORTING WHITE POWDER FROM MY DASHBOARD."
 ];
 
 var text = messageTexts[Math.floor(Math.random() * (messageTexts.length))];
@@ -14,20 +16,22 @@ $( document ).ready(function() {
 
 	generateCipher(function(mapping) {
 		encryptText(mapping, text, function(ciphertext) {
-			$( ".ciphertext" ).html(ciphertext);
+			// $( ".ciphertext" ).html(ciphertext);
+			textlist = text.split("");
 			ciphertextList = ciphertext.split("");
 			ciphertextList.forEach(function(element, index) {
-				if (element == " ") {
-					$(".message").append("<input type='text' disabled class='message-letter space-box' maxlength='1' value=' '>");
-				} 
-				else if (element == ".") {
-					$(".message").append("<input type='text' disabled class='message-letter space-box' maxlength='1' value='.'>");
-				} 
-				else if (element == ",") {
-					$(".message").append("<input type='text' disabled class='message-letter space-box' maxlength='1' value=','>");
+				if (element == " " || element == "." || element == "," || element == "'") {
+					$(".message").append("<div class='message-char'>"
+						+ "<h3 class='cipher-char'>" + ciphertextList[index] + "</h3>"
+						+ "<input type='text' disabled class='message-letter space-box' maxlength='1' value='" 
+						+ element + "'>"
+						+ "</div>");
 				} 
 				else {
-					$(".message").append("<input type='text' class='message-letter' maxlength='1'>");
+					$(".message").append("<div class='message-char'>"
+						+ "<h3 class='cipher-char'>" + ciphertextList[index] + "</h3>"
+						+ "<input type='text' class='message-letter' maxlength='1'>"
+						+ "</div>");
 				}
 			})
 		})
@@ -41,8 +45,21 @@ $( document ).ready(function() {
     $('.test-message').click(function() {
     	var correct = true;
     	var textlist = text.split('');
+    	var correctletters = []
     	$(".message-letter").each(function(index, obj) {
     		if ($(this).val() == textlist[index]) {
+    			correctletters.push($(this).val());
+    			$(this).addClass("correct");
+    			$(this).prop('disabled', true);
+    		} else {
+    			$(this).val("");
+    			// correct = false;
+    		}
+    	})
+    	$(".message-letter").each(function(index, obj) {
+    		if ($.inArray(textlist[index], correctletters) >= 0 || $(this).val() == textlist[index]) {
+    			$(this).val(textlist[index]);
+    			$(this).addClass("correct");
     			$(this).prop('disabled', true);
     		} else {
     			$(this).val("");
@@ -62,14 +79,13 @@ $( document ).ready(function() {
 });
 
 var generateCipher = function(callback) {
-	alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('');
+	alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 	k = Math.floor(Math.random() * (alphanum.length - 1)) + 1;
-	ciphermapping = {" " : " ", "." : ".", "," : ","}
+	ciphermapping = {" " : " ", "." : ".", "," : ",", "'" : "'"}
 	alphanum.forEach(function(element, index) {
 		newIndex = (index + k) % alphanum.length;
 	    ciphermapping[element] = alphanum[newIndex];
 	    if (index == alphanum.length - 1) {
-	    	console.log(ciphermapping);
 	    	callback(ciphermapping);
 	    }
 	});
