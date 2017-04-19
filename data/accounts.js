860a11acdbc7c399c9bbbe6cd78b021c5916aefd
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-04-19 15:52:36 -0400

    small change

--- data/accounts.js
+++ data/accounts.js
@@ -2,7 +2,7 @@ var bank_account = {
   "security question 1": "Who was your first boyfriend/girlfriend?",
   "security answer 1": "Lucy Wang",
   "security question 2": "In what city were you born?",
-  "security answer 2": "Portland",
+  "security answer 2": "Portland, Oregon",
   "security question 3": "What is your favorite movie?",
   "security answer 3": "Mulan",
 };
