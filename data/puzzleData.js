7fb44c1ed042e1afd740f29fc43854e49e0eaf42
nhatn1507 <nvn@mit.edu>
2017-05-02 14:57:27 -0400

    wifi-chatbox integration

--- data/puzzleData.js
+++ data/puzzleData.js
@@ -2,16 +2,19 @@ var puzzleData = [
 	{
 		"ciphertext": "HEY I GOT THE MONEY. I WILL MEET YOU IN THE KENDAL MARIOT HOTEL FOR OUR DEAL. ALSO SWIPE RIGHT ON MY INDER PROFILE.",
 		"wifi": "Mariot Hotel Wi-fi",
+		"location": "Mariot Hotel",
 		"phishing": "inder"
 	},
 	{
 		"ciphertext": "IT IS I, YOUR FAVORITE CUSTOMER. THE DEAL WILL GO DOWN IN THE HYAT HOTEL ON NENORIAL DRIVE. SENT THE INVOICE VIA BOXDROP",
 		"wifi": "Hyat Hotel Guest Wi-fi",
+		"location": "Hyat Hotel",
 		"phishing": "boxdrop"
 	},
 	{
 		"ciphertext": "HEY THANKS FOR THE WEED THE OTHER DAY. MEETING PLACE THIS TIME IS ENDALL SQ HOTEL LOBBY. EMAILED THE INFO TO YOUR DARKMAIL ACCOUNT",
 		"wifi": "Endall Hotel Free Wi-fi",
+		"location": "Endall Hotel",
 		"phishing": "darkmail"
 	}
 ]
