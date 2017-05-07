03208c53f05102ed4d44e65497391539694e92d2
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-05-07 18:05:20 -0400

    added some emails and transactions

--- data/bankData.js
+++ data/bankData.js
@@ -7,37 +7,42 @@ var bankData = {
     },
     'transactions': [
       {
-        "date": "4/30/16",
+        "date": "4/30/17",
         "amount": "-8.23",
         "description": "Peruvian Diner",
       },
       {
-        "date": "4/20/16",
+        "date": "4/20/17",
         "amount": "-500.00",
         "description": "Disneyland Hotel + Theme Park Bundle",
       },
       {
-        "date": "4/10/16",
+        "date": "4/17/17",
+        "amount": "1000000.00",
+        "description": "Donation to the International AIDS Foundation",
+      },
+      {
+        "date": "4/10/17",
         "amount": "10000000.00",
         "description": "Direct Deposit from Bexley Corporation",
       },
       {
-      "date": "3/17/16",
+      "date": "3/16/17",
       "amount": "-200000.00",
       "description": "Donation to #prayforperu Earthquake Relief fund",
       },
       {
-      "date": "3/10/16",
+      "date": "3/10/17",
       "amount": "-500.00",
       "description": "Party City",
       },
       {
-      "date": "3/1/16",
+      "date": "3/1/17",
       "amount": "-30000",
-      "description": "St. Jude Children’s Hospital",
+      "description": "St. Jude Children's Hospital",
       },
       {
-      "date": "2/14/16",
+      "date": "2/14/17",
       "amount": "-500.00",
       "description": "Le Fancy Restaurant",
       },
