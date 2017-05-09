2e96c43394d089017eb3253e6957868f1ad8c244
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-05-09 17:04:25 -0400

    add image sources

---
+++ data/imageSources.js
@@ -0,0 +1,8 @@
+imageSources = [
+  "https://syntheticgems.org/images/Czindex/czwhite.jpg",
+  "http://www.pngall.com/wp-content/uploads/2016/07/Galaxy-PNG-Image.png",
+  "cooltext.com",
+  "http://wallpaper-gallery.net/images/galaxy/galaxy-2.jpg",
+  "http://www.rantlifestyle.com/wp-content/uploads/2014/08/703.-More-Than-One-Dream.jpg",
+  "http://www.freeiconspng.com/uploads/book-icon-by-sonoftroll-26.png",
+];
