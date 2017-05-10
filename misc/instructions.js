fd418b25d04cf06f10b0b6186d1b067b17efac21
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-05-09 23:02:25 -0400

    finished intro

--- misc/instructions.js
+++ misc/instructions.js
@@ -7,4 +7,29 @@ $(function() {
             $('.carousel').carousel('prev');
         }
     });
+
+  var imageUrls = imageSources;
+  for(var i=0; i<fakebookData.length; i++) {
+    imageUrls.push(...fakebookData[i].friends.map(s => s.image));
+    imageUrls.push(...fakebookData[i].posts.map(s => s.image));
+    imageUrls.push(fakebookData[i]['profile image']);
+    imageUrls.push(fakebookData[i]['banner image']);
+  }
+
+  $('.credits').html('CyberHaq was created by Jimmy Zeng, Elise Xue, Kairat Ashim, Raul Boquinr, Nick Nguyen, and Emily Ramirez. Image sources are shown below:<br><br>' + imageUrls.join('<br>'));
+
+  $('#begin-btn').click(function() {
+    location.href = "../index.html";
+  });
+  $('#view-btn').click(function() {
+    if($(this).html() === 'Show Credits') {
+      $('.credits').css({ display: 'block' });
+      $('#credits-hide').css({ display: 'none' });
+      $(this).html('Hide Credits');
+    } else {
+      $('.credits').css({ display: 'none' });
+      $('#credits-hide').css({ display: 'block' });
+      $(this).html('Show Credits');
+    }
+  });
 });
