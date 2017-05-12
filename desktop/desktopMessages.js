e2713eca00f625c745294bf77165d521257c88f8
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-05-11 22:07:09 -0400

    forgot a file

---
+++ desktop/desktopMessages.js
@@ -0,0 +1,15 @@
+$(window).on('message', function(e) {
+  var msg = e.originalEvent.data;
+  if(msg['type'] === 'open-new-tab') {
+    createWindow(msg.site, msg.site, msg.src);
+  } else if(msg['type'] === 'endgame') {
+    var rect = $('<div class="black-rect"></div>').appendTo('body');
+    setTimeout(function() {
+      rect.animate({
+        opacity: 1
+      }, 2000, function() {
+        location.href = "../misc/result.html";
+      });
+    }, 500);
+  }
+});
